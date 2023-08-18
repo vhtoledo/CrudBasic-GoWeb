caa068aa990901baffc866f58329d8f66a440e60
victor toledo <querynetworking@gmail.com>
2023-08-18 13:23:57 -0300

    insertar un registro en la tabla users

--- main.go
+++ main.go
@@ -9,11 +9,15 @@ import (
 func main() {
 	//Verificar conexion
 	db.Connect()
-	db.Ping()
-	fmt.Println(db.ExistsTable("users"))
+	//db.Ping()
+	//fmt.Println(db.ExistsTable("users"))
 
 	//Crear tabla
-	db.CreateTable(models.UserSchema, "users")
+	//db.CreateTable(models.UserSchema, "users")
+
+	// Insertar un registro
+	user := models.CreateUser("victor", "victor123", "victor@gmail.com")
+	fmt.Println(user)
 
 	// Eliminar tabla
 	//db.TruncateTable("users")
--- models/users.go
+++ models/users.go
@@ -1,5 +1,7 @@
 package models
 
+import "gomysql/db"
+
 // Modelo usuario
 type User struct {
 	Id       int64
@@ -16,3 +18,23 @@ const UserSchema string = `CREATE TABLE users(
 	password VARCHAR(64) NOT NULL,
 	email VARCHAR(50),
 	create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`
+
+//Construir usuario
+func NewUser(username, password, email string) *User {
+	user := &User{Username: username, Password: password, Email: email}
+	return user
+}
+
+//Crear usuario e insertar
+func CreateUser(username, password, email string) *User {
+	user := NewUser(username, password, email)
+	user.insert()
+	return user
+}
+
+//Insertar Registro
+func (user *User) insert() {
+	sql := "INSERT users SET username=?, password=?,email=?"
+	result, _ := db.Exec(sql, user.Username, user.Password, user.Email)
+	user.Id, _ = result.LastInsertId()
+}
